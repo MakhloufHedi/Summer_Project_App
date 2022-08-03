d4f291b4c1e3e3a46605d4fab65a53f158d5e57f
MOHAMED <mohamedaloulou702@gmail.com>
2022-08-03 05:21:08 +0100

    4eme video

---
+++ app/src/main/java/com/example/summer_project_app/LoginActivity.java
@@ -0,0 +1,54 @@
+package com.example.summer_project_app;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+    private EditText email,pass;
+    private Button loginB;
+    private TextView forgotPassB, signupB;
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        email=findViewById(R.id.email);
+        pass=findViewById(R.id.password);
+        loginB=findViewById(R.id.loginB);
+        forgotPassB=findViewById(R.id.forgot_pass);
+        signupB=findViewById(R.id.signupB);
+
+        private boolean validateData()
+        {
+            if (email.getText().toString().isEmpty())
+            {
+                email.setError("Enter Email ID");
+                return false;
+            }
+            if (pass.getText().toString().isEmpty())
+            {
+                pass.setError("Enter Password");
+                return false;
+            }
+            return true;
+        }
+        private void login()
+        {
+
+        }
+        loginB.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                if (validateData())
+                {
+                    login();
+                }
+            }
+        });
+
+}
+}
